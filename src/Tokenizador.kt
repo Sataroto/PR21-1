abfc9fcd0a5680fc5b0eb414a9f12bbee70000b0
Sataroto <gera.ay.ju@gmail.com>
2020-10-14 18:23:06 -0500

    Avance

--- src/Tokenizador.kt
+++ src/Tokenizador.kt
@@ -3,8 +3,12 @@ import java.io.File
 public class Tokenizador{
     var tokens : MutableList<Patron> = mutableListOf<Patron>()
 
-    constructor(src :String){
+    constructor(src :String, seleccion: Array<Int>){
         var lines : List<String> = File(src).readLines()
-
+        lines.forEach { line ->
+            if(!line.equals("")) {
+                var muestra: Array<String> = line.split(",").toTypedArray()
+            }
+        }
     }
 }
